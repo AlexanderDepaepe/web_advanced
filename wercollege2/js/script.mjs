f51e58ec77335e81862eecc4016da74bcc3b384c
alexander depaepe <alexander.depaepe@student.ehb.be>
2024-02-26 14:30:27 +0100

    oef3af

--- wercollege2/js/script.mjs
+++ wercollege2/js/script.mjs
@@ -1,1 +1,81 @@
 'use strict';
+// oefening 1
+// const color = {
+//     nR: 0,
+//     nG: 0,
+//     nB: 0,
+
+//     setColor(R,G,B){
+//         this.nR = R;
+//         this.nG = G;
+//         this.nB = B;
+//     },
+//     showColor(){
+//         console.log(`rgb(${this.nR}, ${this.nG}, ${this.nB})`);
+// }
+// };
+
+// color.setColor(12,34,56);
+// color.showColor();
+// color.setColor(255,124,56);
+// color.showColor();
+// color.setColor(255,0,100);
+// color.showColor();
+
+
+//oefening 2
+
+// const color = {
+//     nR: 0,
+//     nG: 0,
+//     nB: 0,
+
+//     setColor(R,G,B){
+//         this.nR = R;
+//         this.nG = G;
+//         this.nB = B;
+//     },
+//     showColor(){
+//         console.log(`rgb(${this.nR}, ${this.nG}, ${this.nB})`);
+// }
+// };
+
+// let R = prompt('Enter a number between 0 and 255 for red');
+// let G = prompt('Enter a number between 0 and 255 for green');
+// let B = prompt('Enter a number between 0 and 255 for blue');
+
+// if (0 < R < 255 || 0 < G <255 || 0 < B <255){
+//     console.log('succes');
+//     color.setColor(R,G,B);
+//     color.showColor();
+// }
+
+//oeferning 3
+// const color = {
+//     nR: 0,
+//     nG: 0,
+//     nB: 0,
+
+//     setColor(R,G,B){
+//         this.nR = R;
+//         this.nG = G;
+//         this.nB = B;
+//     },
+//     showColor(){
+//      const style = 'background-color: rgb('+this.nR+', '+this.nG+', '+this.nB+');font-size: 2em;';
+//      console.log("%cHooray", style);;
+// }
+// };
+
+// let R = prompt('Enter a number between 0 and 255 for red');
+// let G = prompt('Enter a number between 0 and 255 for green');
+// let B = prompt('Enter a number between 0 and 255 for blue');
+
+// if (0 <= R && R <= 255 && 0 <= G && G <= 255 && 0 <= B && B <= 255) {
+//     console.log('succes');
+//     color.setColor(R,G,B);
+//     color.showColor();
+// }
+
+//oefening 4
+
