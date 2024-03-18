b99d7801288a45b7e0a2807420c67799fd53fbf5
alexander depaepe <alexander.depaepe@student.ehb.be>
2024-03-18 11:42:45 +0100

    oef3

--- wercollege4/js/API.js
+++ wercollege4/js/API.js
@@ -1,13 +1,35 @@
-window.onload = () => {
-    console.log('API.js loaded');
-    let promise = fetch('http://www.omdbapi.com/?apikey=ef8ee6fa&t=parasite');
-    promise.then((response) => {
-        return response.json();
-        
-    })
-    .then((data) => {
-        console.log(data);
-    })
+const zoek = document.getElementById('search-form')
 
-    
+zoek.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const zoekTerm = document.getElementById('search-input').value;
+    zoekMovie(zoekTerm);
+});
+
+function zoekMovie(title) {
+    let apiKey = 'ef8ee6fa';
+    let apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`;
+
+    fetch(apiUrl)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            displayMovie(data);
+        })
+       
+}
+
+function displayMovie(data) {
+    const movieDetails = document.getElementById('movie-details');
+    movieDetails.innerHTML = `
+        <h2 class="text-center">${data.Title}</h2>
+        <p class="text-center">Jaar van uitgave: ${data.Year}</p>
+        <p class="text-center">Regisseur: ${data.Director}</p>
+        <p class="text-center">Runtime: ${data.Runtime}</p>
+        <div class="text-center">
+            <img src="${data.Poster}" alt="${data.Title} Poster" style="max-width: 200px;">
+        </div>
+        <p class="text-center">Runtime: ${data.Runtime}</p>
+    `;
 }
